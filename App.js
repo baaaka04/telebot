b5b2d383979f1c830a133ab3f3227adc9e395d3a
ab <ab@sdflkj.csm>
2022-05-09 16:24:36 +0300

    fix formating date to string

--- App.js
+++ App.js
@@ -89,11 +89,11 @@ function writeTodos(arr) {
 function formatDate(unixtime) {
     const jsDate = new Date(unixtime)
 
-    const y = jsDate.getFullYear()
-    const m = (jsDate.getMonth() + 1).toString().padStart(2, '0')
-    const d = jsDate.getDate().toString().padStart(2, '0')
+    const y = jsDate.getUTCFullYear()
+    const m = (jsDate.getUTCMonth() + 1).toString().padStart(2, '0')
+    const d = jsDate.getUTCDate().toString().padStart(2, '0')
     const h = jsDate.getUTCHours().toString().padStart(2, '0')
-    const min = jsDate.getMinutes().toString().padStart(2, '0')
+    const min = jsDate.getUTCMinutes().toString().padStart(2, '0')
     return `${d}-${m}-${y} ${h}:${min}`
 }
 

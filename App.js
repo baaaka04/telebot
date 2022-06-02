c85576114578dbd8899ea84aad71972f258fec2a
ab <ab@sdflkj.csm>
2022-06-02 16:42:28 +0300

    remove Promise for better working

--- App.js
+++ App.js
@@ -5,12 +5,11 @@ const token = '5224733147:AAFaExlVjU0z8a4K2WL3HaqZkN0i79XX3Qo';
 const bot = new TelegramBot(token, { polling: true });
 const chatId = 224688427;
 let remindsQueue = [];
-let remindTimeID;
 
 
 function App() {
 
-    getTodos().map(t =>
+    getTodosFromJSON().map(t =>
         setReminderTimeout(chatId, t, t.datetime, t.frequency)
     )
 
@@ -19,11 +18,7 @@ function App() {
     })
 
     bot.onText(/\/get/, () => {
-        const isRepeatableEvent = (t) => t.repeatable ? ` Повтор каждый ${t.frequency} день` : ''
-        bot.sendMessage(chatId, getTodos()
-            .map((todo, i) =>
-                `${i + 1}. ${todo.message}; ${formatDate(todo.datetime)}${isRepeatableEvent(todo)}\n`)
-            .join(''))
+        sendAllTodos()
     })
 
     bot.onText(/\/add(.+)/, msg => {
@@ -38,17 +33,18 @@ function App() {
             const timeRemind = msg2.text.split(' ')[1]
             const datetimeValue = Date.parse(periodRemind + ' ' + timeRemind) + 3 * 60 * 60 * 1000
             bot.removeListener('message')
+
             bot.sendMessage(chatId, 'Через сколько дней повторить?')
             bot.on('message', msg3 => {
                 const days = +msg3.text
-                const todos = getTodos()
+                const todos = getTodosFromJSON()
                 todos.push({
                     message: msg.text.replace(/\/add./, ''),
                     datetime: datetimeValue,
                     repeatable: days !== 0,
                     frequency: days
                 })
-                writeTodos(todos)
+                writeTodosToJSON(todos)
                 setReminderTimeout(chatId, todos[todos.length - 1], datetimeValue, days)
                 let freqRes = '';
                 days ? freqRes = `Повтор каждый ${days} день` : freqRes
@@ -59,17 +55,21 @@ function App() {
     })
 
     bot.onText(/\/del/, msg => {
-        bot.sendMessage(chatId, getTodos().map((todo, i) => `${i + 1}. ${todo.message}; ${formatDate(todo.datetime)}\n`).join(''))
+        sendAllTodos()
+
         bot.sendMessage(msg.chat.id, 'Введите номер напоминания для удаления:')
         bot.on('message', msg2 => {
             if (/\d+/.test(msg2.text)) {
+
                 let remindIndex = +msg2.text - 1
-                let remindMessage = getTodos()[remindIndex].message
-                bot.sendMessage(chatId, `Удалено: ${remindMessage}`)
-                clearInterval(remindsQueue.find(item => item.name == remindMessage).ID)
+                let remindMessage = getTodosFromJSON()[remindIndex].message
+                const filtered = getTodosFromJSON().filter(item => item.message != remindMessage)
+                writeTodosToJSON(filtered)
+
+                clearTimeout(remindsQueue.find(item => item.name == remindMessage).ID)
                 remindsQueue = remindsQueue.filter(item => item.name != remindMessage)
-                const filtered = getTodos().filter(item => item.message != remindMessage)
-                writeTodos(filtered)
+
+                bot.sendMessage(chatId, `Удалено: ${remindMessage}`)
             } else {
                 bot.sendMessage(chatId, 'Ошибка, повторите попытку.')
             }
@@ -78,14 +78,21 @@ function App() {
     })
 }
 
+function sendAllTodos() {
+    const isRepeatableEvent = (t) => t.repeatable ? ` Повтор каждый ${t.frequency} день` : ''
+    bot.sendMessage(chatId, getTodosFromJSON()
+        .map((todo, i) =>
+            `${i + 1}. ${todo.message}; ${formatDate(todo.datetime)}${isRepeatableEvent(todo)}\n`)
+        .join(''))
+}
 
-function getTodos() {
+function getTodosFromJSON() {
     const source = fs.readFileSync('./todos.json', 'utf-8');
     const todos = JSON.parse(source).source;
     return todos
 }
 
-function writeTodos(arr) {
+function writeTodosToJSON(arr) {
     const todosToAdd = JSON.stringify({ source: arr }, null, 4)
     fs.writeFileSync('./todos.json', todosToAdd)
 }
@@ -101,8 +108,8 @@ function formatDate(unixtime) {
     return `${d}-${m}-${y} ${h}:${min}`
 }
 
-function deleteTodo(dt) {
-    writeTodos(getTodos().filter(t => dt !== t.datetime))
+function deleteTodoJSON(dt) {
+    writeTodosToJSON(getTodosFromJSON().filter(t => dt !== t.datetime))
 }
 
 async function setReminderTimeout(chatId, todo, dt, days) {
@@ -115,28 +122,26 @@ async function setReminderTimeout(chatId, todo, dt, days) {
     console.log('через сколько минут:', waitInMin)
     console.log('-----------------------')
     if (!todo.repeatable) {
-        remindTimeID = setMinutesTimeout(() => {
+        let remindTimeID = setMinutesTimeout(() => {
             bot.sendMessage(chatId, todo.message);
-            deleteTodo(dt);
+            deleteTodoJSON(dt);
             remindsQueue = remindsQueue.filter(item => item.name !== todo.message)
         },
             waitInMin
         )
-        await remindTimeID
         waitInMin > 0 ? remindsQueue.push({ name: todo.message, ID: remindTimeID }) : null
     } else {
-        remindTimeID = setMinutesTimeout(
+        let remindTimeID = setMinutesTimeout(
             () => {
                 bot.sendMessage(chatId, todo.message)
-                let otherTodos = getTodos().filter(t => t.message !== todo.message)
+                let otherTodos = getTodosFromJSON().filter(t => t.message !== todo.message)
                 todo.datetime = getNextDateRemind(dt, days)
                 otherTodos.push(todo)
-                writeTodos(otherTodos)
+                writeTodosToJSON(otherTodos)
                 setReminderTimeout(chatId, todo, getNextDateRemind(dt, days), days)
             },
             waitInMin
         )
-        await remindTimeID
         remindsQueue.push({ name: todo.message, ID: remindTimeID })
     }
 }
@@ -145,24 +150,19 @@ function setMinutesTimeout(callback, minutes) {
     // 60 seconds in a minute
     let msInMin = 60 * 1000;
     let minCount = 0;
-    if (minutes <= 0) {
-        callback.apply(this, [])
-    } else {
-        return new Promise(resolve => {
-            let timer = setInterval(
-                function () {
-                    minCount++;  // a minutes has passed
+    if (minutes <= 0) { callback.apply(this, []) }
+    let timer = setInterval(
+        function () {
+            minCount++;  // a minutes has passed
 
-                    if (minCount == minutes || minutes < 0) {
-                        clearInterval(timer);
-                        callback.apply(this, []);
-                    }
-                },
-                msInMin
-            )
-            resolve(timer)
-        })
-    }
+            if (minCount == minutes || minutes < 0) {
+                clearInterval(timer);
+                callback.apply(this, []);
+            }
+        },
+        msInMin
+    )
+    return timer
 }
 
 function getNextDateRemind(UTCdate, days) {
